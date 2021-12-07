7f8d38423d37489a02e576a8351cdc40f25ff8ab
dr-neptune <mrose4@worcester.edu>
2021-12-07 18:50:31 -0500

    making it less mean, but more correct

--- day7/solution.py
+++ day7/solution.py
@@ -25,12 +25,18 @@ print(min(fuels.values()))
 
 # part 3: mean girls edition
 from statistics import mean
-from math import floor
+from math import floor, ceil
 
-central_point = floor(mean(crabs) - 1/2)
+central_point_floor = floor(mean(crabs))
+central_point_ceil = ceil(mean(crabs))
 
-fuel = 0
-for c in crabs:
-    n = abs(c - central_point)
-    fuel += int((n * (n + 1)) / 2)
-print(fuel)
+central_points = [central_point_floor, central_point_ceil]
+
+solutions = []
+for central_point in central_points:
+    fuel = 0
+    for c in crabs:
+        n = abs(c - central_point)
+        fuel += int((n * (n + 1)) / 2)
+    solutions.append(fuel)
+print(min(solutions))
