6bcec84a752c88c2784f042f3ccebec53324d609
dr-neptune <mrose4@worcester.edu>
2021-12-07 18:25:19 -0500

    should've done this in rust

--- day7/solution.py
+++ day7/solution.py
@@ -3,8 +3,6 @@ from statistics import median
 with open('input.txt') as f:
     crabs = [int(i) for i in f.read().split(',')]
 
-excrabs = [16,1,2,0,4,2,7,1,2,14]
-
 boom_point = int(median(crabs))
 
 fuel = 0
@@ -13,4 +11,14 @@ for c in crabs:
 
 print(fuel)
 
-# part 2
+# part 2, brute force
+fuels = {}
+for i in range(min(crabs), max(crabs) + 1):
+    fuel = 0
+    for c in crabs:
+        # calculate distances
+        n = abs(c - i)
+        fuel += int((n * (n + 1)) / 2)
+    fuels[i] = fuel
+
+print(min(fuels.values()))
