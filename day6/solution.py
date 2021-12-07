d1fd33247d9ff9413d278cb9d4636e7fb06b631e
dr-neptune <mrose4@worcester.edu>
2021-12-06 23:02:36 -0500

    hash em if you got em

--- day6/solution.py
+++ day6/solution.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 from dataclasses import dataclass
 
-
 with open('input.txt') as f:
     fish = [int(i) for i in f.read().split(',')]
-
 
 @dataclass
 class LanternFish:
@@ -13,11 +11,9 @@ class LanternFish:
         if self.life == 0:
             sea.append(LanternFish(9))
             self.life = 6
-            return self
         else:
             self.life -= 1
-            return self
-
+        return self
 
 sea = [LanternFish(i) for i in fish]
 
@@ -29,4 +25,16 @@ for i in range(80):
 print(len(sea))
 
 # part 2
-# that ain't gonna work
+fish_dict = {i:0 for i in range(11)}
+
+for f in fish:
+    fish_dict[f] += 1
+
+for d in range(256):
+    new_swarm = fish_dict[0]
+    fish_dict[7] += new_swarm
+    fish_dict[9] += new_swarm
+    for j in range(10):
+        fish_dict[j] = fish_dict[j+1]
+
+print(sum(fish_dict.values()))
